391da373fb9a4b91f882857ae918d2eaf8c5d152
Gustavo Aquino da Silva <gustavaquinooficial@gmail.com>
2026-03-07 18:47:27 -0300

    feat: add binary operators

--- tests/main.c
+++ tests/main.c
@@ -85,7 +85,7 @@ static void testInterpretChunkVM() {
   freeChunk(&chunk);
 }
 
-static void testNegateArithmeticOperation() {
+static void testNegateArithmeticOperator() {
   initVM();
 
   Chunk chunk;
@@ -99,7 +99,158 @@ static void testNegateArithmeticOperation() {
 
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "testNegateArithmeticOperation");
+  disassembleChunk(&chunk, "testNegateArithmeticOperator");
+
+  interpret(&chunk);
+
+  freeVM();
+
+  freeChunk(&chunk);
+}
+
+static void testBinaryArithmeticOperators() {
+  initVM();
+
+  Chunk chunk;
+
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  disassembleChunk(&chunk, "testBinaryArithmeticOperators");
+
+  interpret(&chunk);
+
+  freeVM();
+
+  freeChunk(&chunk);
+}
+
+static void testOtherBinaryArithmeticOperators() {
+  initVM();
+
+  Chunk chunk;
+
+  initChunk(&chunk);
+
+  // 1 * 2 + 3 = 5
+  int constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  interpret(&chunk);
+
+  freeChunk(&chunk);
+
+  // 1 + 2 * 3 = 7
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  interpret(&chunk);
+
+  freeChunk(&chunk);
+
+  // 3 - 2 - 1 = 0
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  interpret(&chunk);
+
+  freeChunk(&chunk);
+
+  // 1 + 2 * 3 - 4 / -5 = 7.8
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  constant = addConstant(&chunk, 5);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_NEGATE, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+
+  writeChunk(&chunk, OP_SUBTRACT, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
 
   interpret(&chunk);
 
@@ -119,7 +270,13 @@ static void run_vm_tests() {
   testInterpretChunkVM();
   putchar('\n');
 
-  testNegateArithmeticOperation();
+  testNegateArithmeticOperator();
+  putchar('\n');
+
+  testBinaryArithmeticOperators();
+  putchar('\n');
+
+  testOtherBinaryArithmeticOperators();
   putchar('\n');
 
   printf("VM tests executed.\n");
